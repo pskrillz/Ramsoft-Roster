6781c36a391237458e187aa2b610df5dda198df0
pi <payam.farah@live.com>
2021-08-10 22:03:37 -0700

    added configuration for demo

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  url: 'https://softrams-api.herokuapp.com/api',
+  environmentName: "demo"
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  url: 'https://softrams-api.herokuapp.com/api'
+  // url: 'https://softrams-api.herokuapp.com/api'
+  url: 'http://localhost:8000/api'
 };
